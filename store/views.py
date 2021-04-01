53472d8900b4ef66c363387d2cfceaebfb044b48
Israel Abraham <israelvictory87@gmail.com>
2021-04-01 13:39:44 +0100

    Initialized store/views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    pass
+
+
+def register(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def dashboard(request):
+    pass
+
+
+def dealers(request):
+    pass
+
+
+def medicines(request):
+    pass
+
+
+def employees(request):
+    pass
+
+
+def customers(request):
+    pass
+
+
+def purchases(request):
+    pass
