1bc55b67c4653bddf4c0c44b0e09a374f98b9405
Israel Abraham <israelvictory87@gmail.com>
2021-04-02 12:33:29 +0100

    Add field med_name to purchase and remove field prod_name from purchase

--- store/models.py
+++ store/models.py
@@ -13,7 +13,7 @@ class Medicine(models.Model):
     med_code = models.IntegerField()
     med_name = models.CharField(max_length=50)
     dealer_name = models.ForeignKey(Dealer, on_delete=models.CASCADE)
-    price = models.IntegerField()
+    price = models.FloatField()
     stock = models.IntegerField()
     description = models.TextField()
 
@@ -23,7 +23,7 @@ class Employee(models.Model):
     fname = models.CharField(max_length=100)
     lname = models.CharField(max_length=100)
     address = models.TextField()
-    salary = models.IntegerField()
+    salary = models.FloatField()
     phone_number = models.CharField(max_length=15)
     email = models.CharField(max_length=20)
 
@@ -37,7 +37,7 @@ class Customer(models.Model):
 
 
 class Purchase(models.Model):
-    prod_name = models.CharField(max_length=100)
+    med_name = models.ForeignKey(Medicine, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    price_number = models.IntegerField()
+    price_number = models.FloatField()
     quantity = models.IntegerField()
