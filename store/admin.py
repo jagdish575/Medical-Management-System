a5567661b56a54ecacad31691e64b2a95df244d4
Israel Abraham <israelvictory87@gmail.com>
2021-04-02 12:33:42 +0100

    Created models Admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Dealer, Medicine, Employee, Customer, Purchase
 
-# Register your models here.
+class DealerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'fname', 'lname',
+        'address', 'phone_number', 'email'
+    ]
+
+
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'med_code', 'med_name', 'dealer_name',
+        'price', 'stock', 'description'
+    ]
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'emp_id', 'fname', 'lname',
+        'address', 'salary', 'phone_number', 'email'
+    ]
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'fname', 'lname', 'address',
+        'phone_number', 'email'
+    ]
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'med_name', 'customer',
+        'price_number', 'quantity'
+    ]
+
+
+admin.site.register(Dealer, DealerAdmin)
+admin.site.register(Medicine, MedicineAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
