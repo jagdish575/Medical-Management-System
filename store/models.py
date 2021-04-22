fa6307607f12e7608bf17e86f91ecd9a9e388907
Israel Abraham <israelvictory87@gmail.com>
2021-04-22 03:21:56 +0100

    Alter field email on Dealer model

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Dealer(models.Model):
     lname = models.CharField(max_length=100)
     address = models.TextField()
     phone_number = models.CharField(max_length=15)
-    email = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
 
     def __str__(self):
         return "{self.fname} {self.lname}"
