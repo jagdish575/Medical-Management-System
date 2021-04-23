4a0660a629aa4208fcb026e050f21a03ecf93d4d
Israel Abraham <israelvictory87@gmail.com>
2021-04-23 07:49:39 +0100

    Update Profile model

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Dealer(models.Model):
@@ -97,3 +99,22 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    # Resize User Profile Picture 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save()
+        profile_picture = Image.open(self.profile_picture.path)
+        if profile_picture.height > 300 or profile_picture.width > 300:
+            output_size = (300, 300)
+            profile_picture.thumbnail(output_size)
+        print(profile_picture)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
